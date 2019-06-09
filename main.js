427c22805ad59c258721fa6ccbcc15c1ea24aa07
Shree Patel <36641237+ShreePatel95@users.noreply.github.com>
2019-06-08 23:38:27 -0400

    added javascript  to hamburger button

---
+++ main.js
@@ -0,0 +1,10 @@
+let hamburger = document.getElementById("hamburger-dropdown");
+let hamburgerDropdown = document.querySelector(".hamburger-dropdown");
+hamburger.addEventListener("click", function(){
+    console.log("is");
+    if(hamburgerDropdown.style.display === "none"){
+        hamburgerDropdown.style.display = "block";
+    } else{
+        hamburgerDropdown.style.display = "none";
+    }
+});
