b74cc2096060588c55cf529f98cb755a3dd53f82
Shree Patel <36641237+ShreePatel95@users.noreply.github.com>
2019-07-02 21:42:34 -0400

    added mobile sidebar nav and styles

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-let hamburger = document.getElementById("hamburger-dropdown");
-let hamburgerDropdown = document.querySelector(".hamburger-dropdown");
+let hamburger = document.querySelector(".nav-hamburger-button");
+let mobile_sidebar = document.querySelector(".mobile-sidebar");
+let close_button = document.querySelector(".close-button");
+
 hamburger.addEventListener("click", function(){
-    if(hamburgerDropdown.style.display === "none"){
-        hamburgerDropdown.style.display = "block";
-    } else{
-        hamburgerDropdown.style.display = "none";
-    }
+    mobile_sidebar.style.width = "250px";
+});
+
+close_button.addEventListener("click", function(){
+    mobile_sidebar.style.width = "0px";
 });
